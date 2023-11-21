f2255757cb0ae054ee7b7c04a1d4105aaf618c7c
DylanNavarro97 <dylanrubennavarro@hotmail.com>
2023-11-20 23:40:56 -0300

    ejercicios 2 y 3

--- js/ej1.js
+++ js/ej1.js
@@ -1,14 +1,3 @@
-// 1- Crea un objeto llamado auto que tenga algunas características como el color, marca, modelo y si está encendido o apagado. Crea los métodos necesarios para permitir encender y apagar el auto.
-
-// Output:
-
-
-
-// objeto.encender();
-// objeto.apadar()
-// auto encendido
-// El auto se apagó
-
 const auto = {
     color: 'blanco',
     marca: 'Ford',
